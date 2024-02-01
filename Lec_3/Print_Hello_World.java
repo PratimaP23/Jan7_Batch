d13390c5f701d654c36a46ea9cc5994efee50be1
PratimaP23 <156713745+PratimaP23@users.noreply.github.com>
2024-02-01 20:28:21 +0530

    Add files via upload

---
+++ Lec_3/Print_Hello_World.java
@@ -0,0 +1,15 @@
+package Lec_3;
+
+public class Print_Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+		System.out.print("World"+" ");
+		System.out.println("Bye");
+		System.out.print("World");
+
+	}
+
+}
